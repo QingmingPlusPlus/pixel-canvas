f6aa856035178204cdb7f601ac86dac8a2433d1b
qingming++ <50537788+QingmingPlusPlus@users.noreply.github.com>
2026-01-12 22:10:34 +0800

    共享内存demo

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,33 @@
 use wasm_bindgen::prelude::*;
 
+/// SharedBuffer - 一个可以在 Rust 和 JS 之间共享的内存区域
 #[wasm_bindgen]
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct SharedBuffer {
+    buffer: Vec<u8>,
+}
+
+#[wasm_bindgen]
+impl SharedBuffer {
+    /// 创建一个新的 SharedBuffer，分配指定大小的内存
+    #[wasm_bindgen(constructor)]
+    pub fn new(size: usize) -> SharedBuffer {
+        SharedBuffer {
+            buffer: vec![0; size],
+        }
+    }
+
+    /// 返回 buffer 的指针，供 JS 端访问
+    pub fn ptr(&self) -> *const u8 {
+        self.buffer.as_ptr()
+    }
+
+    /// 返回 buffer 的长度
+    pub fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    /// 刷新 buffer，填充随机值
+    pub fn refresh(&mut self) {
+        getrandom::getrandom(&mut self.buffer).expect("Failed to generate random bytes");
+    }
 }
